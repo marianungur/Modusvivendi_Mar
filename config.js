ce1c26e847b21d416bf220a9dd64b731568c0934
marianungur <marianungur@gmail.com>
2021-10-04 00:05:43 +0300

    04

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"","author":"","subject":"","keywords":"","creator":"","producer":"","creationdate":"","moddate":"","trapped":"","fileName":"MODUSvivendi_Vitalie_Antigen.pdf","bounds":[[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
